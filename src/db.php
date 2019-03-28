cff8a6856e8dc97bbbbd8b5c06f341c7f38969ca
Marcin Marcin <marcin22x@gmail.com>
2019-03-28 09:41:11 +0100

    Tekst wiadomoci to teraz TEXT()

--- src/db.php
+++ src/db.php
@@ -6,7 +6,7 @@
             msg_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT ,
             uname VARCHAR(32) NOT NULL ,
             timestamp TIMESTAMP NOT NULL ,
-            message VARCHAR(256) NOT NULL ,
+            message TEXT(256) NOT NULL ,
             PRIMARY KEY(msg_id)    
         );
     */
