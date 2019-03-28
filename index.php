8e3119743d766deb64055aab83ea255a830203de
Marcin Marcin <marcin22x@gmail.com>
2019-03-28 09:40:22 +0100

    Wersja dziaajaca

--- index.php
+++ index.php
@@ -7,26 +7,26 @@
         <title>Czat</title>
     </head>
     <body>
-
 <?php
     // Tworzenie połączenia z bazą danych
-    $self       = $_SERVER['PHP_SELF'];
-    $address    = $_SERVER['REMOTE_ADDR'];
+    $self = $_SERVER['PHP_SELF'];
 
     // Dodanie formularza
-    echo
-        '<form action="' , $self , '">
-            <p>Napisz coś:</p>
-            <input name="nazwa" type="text" cols="25"/>
+    echo '
+        <form method="POST" action="' , $self , '">
+            <p>Twoja nazwa:</p><br/>
+            <input name="name" type="text" cols="25"/>
+            <p>Wiadomość:</p><br/>
             <textarea name="txt" rows=5 cols="25"></textarea>
-            <input name="send" type="hidden"/>
+            <input name="send" type="hidden"/></br>
             <input type="submit" value="Wyślij!"/>
-        </form>';
+        </form>
+        ';
 
     include('src/db.php');
 
     $mysqli = new mysqli($db_host , $db_user , $db_passwd , $db_name);
-    
+
     if ($mysqli->connect_errno)
         die('<p class="error">DB Select Error!</p>');
 
@@ -36,32 +36,29 @@
         if (empty($_POST['name']) || empty($_POST['txt']))
         {
             echo
-        '<script>alert("Pola formularza nie mogą być puste!");</script>';
+        '<p class="error">Pola formularza nie mogą być puste!</p>';
         }
 
         // Wysyłanie wiadomości na serwer
         else
         {
-            $user = htmlspecialchars(mysql_real_escape_string($_POST['name']));
-            $text = htmlspecialchars(mysql_real_escape_string($_POST['message']));
+            $user = htmlspecialchars($mysqli->real_escape_string($_POST['name']));
+            $text = htmlspecialchars($mysqli->real_escape_string($_POST['txt']));
 
-            if (!$mysqli->query('INSERT INTO ' , $db_tab , ' SET uname=\'' , $user , '\', message=\'', $text , '\', timestamp=NOW();'))
+            if (!$mysqli->query('INSERT INTO ' . $db_tab . ' SET uname=\'' . $user . '\', message=\''. $text . '\', timestamp=NOW();'))
             {
                 echo
-        '<script>alert("Nie można wysłać wiadomości!");</script>';
+         '<p class="error">Nie wysłano wiadomości!</p>';
             }
 
         }
-    }
-    else
-    {
     }
 
     // Pobieranie wiadomości
     $result = $mysqli->query('SELECT * FROM ' . $db_tab . ' ORDER BY msg_id DESC LIMIT 50;');
 
-    echo
-        '<ul>';
+    echo '
+        <ul>';
 
     // Przetwarzanie wyniku
     while ($tmp = $result->fetch_array())
@@ -70,19 +67,21 @@
         $res_msg = stripslashes($tmp['message']);
 
         // Dodawanie wiadomości
-        echo
-            '<li>
+        echo '
+            <li>
                 <span>#' , $tmp['msg_id'] , '</span>
                 <span>' , $res_name , '</span>
                 <span>' , $tmp['timestamp'] , '</span>
                 <span>' , $tmp['message'] , '</span>
-            </li>';
+            </li>
+            ';
     }
 
     $result->free();
     $mysqli->close();
 
 ?>
+
         </ul>
     </body>
 </html>
